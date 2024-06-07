7686925d93facb92b92654ee8c5208f78cf94323
Vitaliy230888 <slam20061@yandex.ru>
2024-06-07 13:11:37 +0300

    Add files via upload

---
+++ task0108.java
@@ -0,0 +1,18 @@
+package task0108;
+
+public class task0108 {
+    public static void main(String[] args) {
+            int а = 3;
+            int б = 2;
+//            System.out.print("два");
+            System.out.print( б );
+            System.out.print(" плюс ");
+//            System.out.print("минус");
+            System.out.print( а );
+//            System.out.print("три");
+            System.out.print(" равно ");
+//            System.out.print("будет");
+//            System.out.print("пять");
+            System.out.print(а+б);
+        }
+    }
